03cf62d910dc39256ba2131cc6c5c6fdf5b6ec73
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 19:25:26 +0100

    own version number

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # re-native-splash-screen
+
+A Clojurescript library for [react-native-splash-screen](https://github.com/crazycodeboy/react-native-splash-screen)
+Meant for use with reagent and re-frame
+
+Tracking       | Artifact
+---------------|---------|
+`v2.0.0`       | `[re-native/splash-screen "0.0.1-SNAPSHOT"]`
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-native/splash-screen "2.0.0"
+(defproject re-native/splash-screen "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
                  [org.clojure/clojurescript "1.9.473"]
                  [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
