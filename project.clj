ff6f275fcc7ba589c941e6e167a1d0f03ad0ce3c
Fabian Aussems <fabianaussems@gmail.com>
2017-03-17 18:26:27 +0100

    move re-native to re_native

--- project.clj
+++ project.clj
@@ -1,1 +1,6 @@
-(defproject re-native/splash-screen "2.0.0")
+(defproject re-native/splash-screen "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [reagent "0.6.0" :exclusions [cljsjs/react cljsjs/react-dom cljsjs/react-dom-server]]
+                 [re-frame "0.9.1"]]
+  :plugins [[lein-cljsbuild "1.1.4"]])
